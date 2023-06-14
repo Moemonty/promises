07a88e5caac1bded66df87711e6619d55abf3f4e
Moe Montenegro <moe@senecafoundation.org>
2023-06-14 16:59:34 -0500

    Success and error handling events

--- promise.js
+++ promise.js
@@ -1,7 +1,34 @@
 console.log('Hello');
 
-const promise = fetch("https://json.versant.digital/.netlify/functions/fake-api/message")
+const promise = fetch("https://json.versant.digital/.netlify/functions/fake-api/message");
+const failedPromise = fetch("https://json.versant.digitaxxl/.netlify/functions/fake-apxr/messagexx");
 
-promise.then( res => console.log(res.status));
-promise.then( res => console.log(res.type));
-// promise.then( res => console.log(res));
+promise.then(
+    (res) => {
+        console.log('Success!');
+        console.log(res.type);
+        console.log('-------------------------');
+    },
+    (res) => {
+        console.log(res.type)
+        console.log('error');
+    }
+);
+
+failedPromise.then(
+    (res) => {
+        console.log('-------------------------');
+        console.log('Success!')
+        console.log(res);
+        console.log(res.type)
+    },
+    (res) => {
+        console.log('Promise not fullfilled');
+        console.log(res.message);
+        console.log(res.stack);
+        console.log('-------------------------')
+    }
+);
+
+
+
