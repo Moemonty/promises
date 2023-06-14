f0633d2babf0a35a8bff6131495ca7e8806ebbc3
Moe Montenegro <moe@senecafoundation.org>
2023-06-14 17:15:25 -0500

    Add settlement handlers

--- promise.js
+++ promise.js
@@ -1,6 +1,7 @@
 console.log('Hello');
 
 const promise = fetch("https://json.versant.digital/.netlify/functions/fake-api/message");
+const notOkPromise = fetch("https://json.versant.digital/.netlify/functions/fake-api/messageXXX");
 const failedPromise = fetch("https://json.versant.digitaxxl/.netlify/functions/fake-apxr/messagexx");
 
 promise.then(
@@ -13,7 +14,7 @@ promise.then(
         console.log(res.type)
         console.log('error');
     }
-);
+).finally(() => console.log('Finally, called at end!'));
 
 failedPromise.then(
     (res) => {
@@ -31,4 +32,14 @@ failedPromise.then(
 );
 
 
+// Gerard Way Status Checking
+notOkPromise.then(res => {
+    if (res.ok) {
+        console.log('Response okay!')
+    } else {
+        console.log('Trust me, I\'m not okay');
+        console.log('-----------------------------')
+    }
+});
 
+failedPromise.catch(res => console.log(res.message));
